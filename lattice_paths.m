5ed48951fef8e5c42fa80d3d1cfa0156500f744f
Shengfeng Chen <mike.chen1632@gmail.com>
2018-03-20 01:37:42 -0400

    Update lattice_paths.m

--- lattice_paths.m
+++ lattice_paths.m
@@ -1,4 +1,8 @@
-a=20;
+% PE015 Lattice paths
+
+% Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+a = 20;
 s = 2:a+1;
 for j = 1:a-1
     for i = 1:a
